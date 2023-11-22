fce2b2517210026e287453f2a3dc56e73088e83d
2kyunghee <lxxkyung@onflou.com>
2023-11-22 17:29:29 +0900

    feat: react-kakao-login 구현

--- src/components/member/Login/Kakao/KakaoLogin.js
+++ src/components/member/Login/Kakao/KakaoLogin.js
@@ -9,8 +9,8 @@ const KakaoLogin = () =>{
     // 카카오 로그인 함수를 실행 시키면 아래에 설정해 놓은 KAKAO_AUTH_URL 주소로 이동.
     // 이동 된 창에서 kakao 계정 로그인을 시도할 수 있으며 로그인 버튼 클릭 시 Redirect URI로 이동하면서 빈 화면과 함께 인가코드가 발급됨.
     // 인가코드는 파라미터 값에 들어가 있음 !
-    const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-    const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+    const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
+    const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
     const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`
 
     const onClickEvent = () =>{
@@ -20,7 +20,7 @@ const KakaoLogin = () =>{
     return (
         <>
         <StKakaoLogin onClick={onClickEvent}>kakao 로그인</StKakaoLogin>
-        <KakaoLoginButton REST_API_KEY={REST_API_KEY}/>
+        <KakaoLoginButton/>
         </>
     )
 }
@@ -28,10 +28,10 @@ const KakaoLogin = () =>{
 export default KakaoLogin;
 
 const StKakaoLogin = styled.button`
-    width: 200px;
-    height: 100px;
+    width: 100px;
+    height: 50px;
     border-radius: 10px;
     border: 1px solid #000;
-    margin-top: 20px;
+    margin: 20px 0;
     
 `
--- src/components/member/Login/Kakao/KakaoLoginButton.js
+++ src/components/member/Login/Kakao/KakaoLoginButton.js
@@ -2,23 +2,24 @@ import { useEffect } from "react";
 import KakaoLogin from "react-kakao-login";
 
 //react-kakao-login 이용한 카카오 로그인
-const KakaoLoginButton = ({REST_API_KEY}) =>{
+const KakaoLoginButton = () =>{
+    const KAKAO_TOKEN_KEY = process.env.REACT_APP_KAKAO_API_KEY;
 
     const responseKaKao = (response) =>{
         console.log(response)
     }
 
-    useEffect (() => {
-        // Kakao JavaScript SDK 초기화
-        if (!window.Kakao.isInitialized()) {
-            window.Kakao.init(REST_API_KEY);
-          }
-    }, [])
+    // useEffect (() => {
+    //     // Kakao JavaScript SDK 초기화
+    //     if (!window.Kakao.isInitialized()) {
+    //         window.Kakao.init(KAKAO_TOKEN_KEY);
+    //       }
+    // }, [])
 
     return (
      <div>
         <KakaoLogin
-            token={String(REST_API_KEY)}
+            token={KAKAO_TOKEN_KEY}
             onSuccess={responseKaKao}
             onFail={(err) => console.log("로그인 실패", err)}
             onLogout={() => console.log("로그아웃")}
